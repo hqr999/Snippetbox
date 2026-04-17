5c8a301733176b55b72c82fc0037a4407ce8f73e
Henrique Queiroz Reuter <henriquereuter46@gmail.com>
2026-04-16 22:03:32 -0300

    Injecting dependencies with an application struct

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,45 +2,46 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 	"text/template"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+
+// Change the signature of the home handler so it is defined as a method against
+// *application.
+func(app *application) home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
 	
-	// Initialize a slice containing the paths to the two files. It's important
-	// to note that the file containing our base template must be the *first*
-	// file in the slice.
 	files_slice := []string{
 				"./ui/html/base.tmpl",
 				"./ui/html/pages/home.tmpl",
 				"./ui/html/partials/nav.tmpl",
 	}
 
-
-	// Use the template.ParseFiles() function to read the files and store the
-	// templates in a template set. Notice that we use ... to pass the contents
-	// of the files slice as variadic arguments.
 	ts, err := template.ParseFiles(files_slice...)
 	if err != nil {
-		log.Print(err.Error())
+		// Because the home handler is now a method against the application
+	// struct it can access its fields, including the structured logger. We'll
+	// use this to create a log entry at Error level containing the error
+	// message, also including the request method and URI as attributes to
+	// assist with debugging.	
+		app.logger.Error(err.Error(),"method",r.Method,"url",r.URL.RequestURI())
 		http.Error(w,"Internal Server Error",http.StatusInternalServerError)
 		return 
 	}
 
-	// Use the ExecuteTemplate() method to write the content of the "base"
-	// template as the response body.
 	err = ts.ExecuteTemplate(w,"base",nil)
 	if err != nil {
-		log.Print(err.Error())
+		// And we also need to update the code here to use the structured logger too.
+		app.logger.Error(err.Error(),"method",r.Method,"url",r.URL.RequestURI())
 		http.Error(w,"Internal Server Error",http.StatusInternalServerError)
 	}
 }
 
-func snippetView(w http.ResponseWriter, r *http.Request) {
+// Change the signature of the snippetView handler so it is defined as a method
+// against *application.
+func(app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.PathValue("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
@@ -50,11 +51,17 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
+
+// Change the signature of the snippetCreate handler so it is defined as a method
+// against *application.
+func(app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display a form for creating a new snippet..."))
 }
 
-func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+
+// Change the signature of the snippetCreatePost handler so it is defined as a method
+// against *application.
+func(app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte("Save a new snippet..."))
 }
