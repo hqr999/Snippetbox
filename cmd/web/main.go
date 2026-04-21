a36361d90f202b56bbfceb1a306570546d2bc08f
Henrique Queiroz Reuter <henriquereuter46@gmail.com>
2026-04-21 18:56:10 -0300

    The mux with all of our routes is now isolated on a separate file, we just call the routes() function on our main.go

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -20,27 +20,17 @@ func main() {
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{AddSource: true}))
 
-	// Initialize a new instance of our application struct, containing the
-	// dependencies (for now, just the structured logger).
 	app := &application{
 		logger: logger,
 	}
 
-	mux := http.NewServeMux()
-
-	file_server := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("GET /static/", http.StripPrefix("/static", file_server))
-
-	// Swap the route declarations to use the application struct's methods as the
-	// handler functions.
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
 	logger.Info("starting server", "addr", *addr)
 
-	err := http.ListenAndServe(*addr, mux)
+
+	// Call the new app.routes() method to get the servemux conta	ining our routes,
+	// and pass that to http.ListenAndServe().
+	err := http.ListenAndServe(*addr, app.routes())
 
 	logger.Error(err.Error())
 	os.Exit(1)
