54af68bd639dbd22fa0b5d0c4505b33a45998c86
Henrique Queiroz Reuter <henriquereuter46@gmail.com>
2026-05-12 21:23:16 -0300

    We have set a basic way to display dynamic templates on our HTML application

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,28 +3,28 @@ package main
 import (
 	"errors"
 	"fmt"
+	"html/template"
 	"net/http"
 	"strconv"
-	//"html/template"
 
 	"github.com/hqr999/Snippetbox/internal/models"
 )
 
-// Change the signature of the home handler so it is defined as a method against
-// *application.
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
 
+	//To render snippets the Latest snippets on the home page
+	/*
 	snippets, err := app.snippets.Latest()
 	if err != nil {
-		app.serverError(w,r,err)
+		app.serverError(w, r, err)
 		return
 	}
 
 	for _, snippet := range snippets {
-		fmt.Fprintf(w,"%+v\n",snippet)
-	}
-	/*files_slice := []string{
+		fmt.Fprintf(w, "%+v\n", snippet)
+	}*/
+	files_slice := []string{
 		"./ui/html/base.tmpl",
 		"./ui/html/pages/home.tmpl",
 		"./ui/html/partials/nav.tmpl",
@@ -32,19 +32,17 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 	ts, err := template.ParseFiles(files_slice...)
 	if err != nil {
-		app.serverError(w, r, err) //Use the serverError() helper
+		app.serverError(w, r, err)
 		return
 	}
 
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		app.serverError(w, r, err) //Use the serverError() helper
-	}*/
+		app.serverError(w, r, err)
+	}
 
 }
 
-// Change the signature of the snippetView handler so it is defined as a method
-// against *application.
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.PathValue("id"))
 	if err != nil || id < 1 {
@@ -52,9 +50,6 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Use the SnippetModel´s Get() method to retrieve the data for a
-	// specific record based on its ID. If no matching record is found,
-	// return a 404 Not Found response.
 	snippet, err := app.snippets.Get(id)
 	if err != nil {
 		if errors.Is(err, models.ErrNoRecord) {
@@ -65,7 +60,32 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "%+v", snippet)
+	//Initialize a slice containing the paths to the view.tmpl file.
+	// plus the base layout and navigation partial that we made earlier.
+	files_path := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/view.tmpl",
+	}
+
+	//Parse the template files
+	ts, err := template.ParseFiles(files_path...)
+
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	//Create an instance of a templateData struct holding the snippet data.
+	data := templateData{
+		Snippet: snippet,
+	}
+
+	// Pass in the templateData struct when executing the template.
+	err = ts.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		app.serverError(w, r, err)
+	}
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
