0fe30c08a7535f34b4678abc4f79cf7104417000
Henrique Queiroz Reuter <henriquereuter46@gmail.com>
2026-06-02 18:27:04 -0300

    Made our create SnippetCreatePost check for unfilled parts of our form

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
 	"strconv"
-	"fmt"
+	"strings"
+	"unicode/utf8"
+
 	"github.com/hqr999/Snippetbox/internal/models"
 )
 
@@ -55,39 +58,58 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-		// First we call r.ParseForm() which adds any data in Post request bodies
-		// to the r.PostForm map. This also works in the same way for PUT and PATCH
-		// requests. If there are any errors, we use our app.ClientError() helper to
-		// send a 400 Bad Request response to the user.
-		err := r.ParseForm()
-		if err != nil {
-				app.clientError(w,http.StatusBadRequest)
-				return 
-		}
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
 
-		// Use the r.PostForm.Get() method to retrieve the title and content 
-		// from the r.PostForm map. 
-		title_snippet   := r.PostForm.Get("title")
-		content_snippet := r.PostForm.Get("content") 
+	title_field := r.PostForm.Get("title")
+	content_field := r.PostForm.Get("content")
 
-		// The r.PostForm.Get() method always returns the form data as a *string*.
-		// However, we are expecting our expires value to be a number, and want to 
-		// represent it in our Go code as an integer. So we need to manually convert 
-		// the form data to an integer using strconv.Atoi(), and send a 400 Bad
-		// Request response if the conversion falls.
-		expiration_snippet, err := strconv.Atoi(r.PostForm.Get("expires"))
-		if err != nil {
-			app.clientError(w,http.StatusBadRequest)
-			return 
+	expiration_field_ops, err := strconv.Atoi(r.PostForm.Get("expires"))
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
 
-		}
+	}
+	// Initialize a map to hold any validation errors fir the form fields
+	formFieldErrors := make(map[string]string)
 
-		id, err := app.snippets.Insert(title_snippet,content_snippet,expiration_snippet)
-		if err != nil {
-				app.serverError(w,r,err)
-				return
-		}
-	
-		http.Redirect(w,r,fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
+	// Check that the title value is not blank and is
+	// not more than 100 characters of lenght. If it
+	// falls either of those checks, add a message
+	// to the errors map using the field name as the key.
+	if strings.TrimSpace(title_field) == "" {
+		formFieldErrors["title"] = "This field cannot be blank"
+	} else if utf8.RuneCountInString(title_field) > 100 {
+		formFieldErrors["title"] = "This field cannot be more than 100 characters long"
+	}
+
+	// Check that the content value isn´t blank
+	if strings.TrimSpace(content_field) == "" {
+		formFieldErrors["content"] = "This field can´t be blank"
+	}
+
+	// Check the expires value matches of the permitted values
+	// (1, 7, or 365)
+	if expiration_field_ops != 1 && expiration_field_ops != 7 && expiration_field_ops != 365 {
+		formFieldErrors["expiration option"] = "This field must be equal to either 1, 7 or 365."
+	}
+
+	// If there are any errors, dump them in a plain.Text HTTP response and
+	// return from the handler.
+	if len(formFieldErrors) > 0 {
+		fmt.Fprint(w, formFieldErrors)
+		return
+	}
+
+	id, err := app.snippets.Insert(title_field, content_field, expiration_field_ops)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 
 }
