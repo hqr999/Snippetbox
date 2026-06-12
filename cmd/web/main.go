16d49ccacaf5e19dd67e0fcfde28afb3329778ab
Henrique Queiroz Reuter <henriquereuter46@gmail.com>
2026-06-11 21:47:06 -0300

    Applied our self signed certificates to our http Server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,18 +11,18 @@ import (
 
 	"github.com/hqr999/Snippetbox/internal/models"
 
-	"github.com/alexedwards/scs/mysqlstore" 	
-	"github.com/alexedwards/scs/v2"         
+	"github.com/alexedwards/scs/mysqlstore"
+	"github.com/alexedwards/scs/v2"
 	"github.com/go-playground/form/v4"
 	_ "github.com/go-sql-driver/mysql"
 )
 
-//Add a new sessionManager field to the application struct
+// Add a new sessionManager field to the application struct
 type application struct {
-	logger *slog.Logger
-	snippets *models.SnippetModel
-	templateCache map[string]*template.Template
-	formDecoder *form.Decoder 
+	logger         *slog.Logger
+	snippets       *models.SnippetModel
+	templateCache  map[string]*template.Template
+	formDecoder    *form.Decoder
 	sessionMangaer *scs.SessionManager
 }
 
@@ -31,9 +31,7 @@ func main() {
 	dsn := flag.String("dsn", "web:gintoki@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
-	
 	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{AddSource: true}))
-	
 
 	db, err := openDB(*dsn)
 	if err != nil {
@@ -48,44 +46,38 @@ func main() {
 		logger.Error(err.Error())
 		os.Exit(1)
 	}
-	
+
 	formDecoder := form.NewDecoder()
 
 	sessionMan := scs.New()
 	sessionMan.Store = mysqlstore.New(db)
 	sessionMan.Lifetime = 12 * time.Hour
-
-
-
+	//Make sure the Secure attribute is set on our sessions cookies.
+	//Setting this means that the cookie will only be sent by a user´s web
+	//browser when an HTTPS connection is being used (and won´t be sent over an
+	//unsecure HTTP connection).
+	sessionMan.Cookie.Secure = true
 
 	app := &application{
-		logger: logger,
-		snippets: &models.SnippetModel{DB: db},
-		templateCache: cachePageTmpl,
-		formDecoder: formDecoder,
+		logger:         logger,
+		snippets:       &models.SnippetModel{DB: db},
+		templateCache:  cachePageTmpl,
+		formDecoder:    formDecoder,
 		sessionMangaer: sessionMan,
 	}
 
-	//Initialize a new http.Server struct. We set the Addr and Handler fields type so 
-	//that the server uses the same network address and routes as before
-
 	server := &http.Server{
-		Addr: *addr,
-		Handler: app.routes(),
-		//Create a *log.Logger from our structured logger handler, which writes 
-		//log entries at the Error level, and assign it to the ErrorLog field. If 
-		//you would prefer to log on the server errors at Warn level instead, you 
-		//could pass slog.LevelWarn as the final paramter. 
-		ErrorLog: slog.NewLogLogger(logger.Handler(),slog.LevelError),
-			
+		Addr:     *addr,
+		Handler:  app.routes(),
+		ErrorLog: slog.NewLogLogger(logger.Handler(), slog.LevelError),
 	}
 
 	logger.Info("starting server", "addr", server.Addr)
 
-	
-	//Call the ListenAndServe() method on our new http.Server struct to start
-	//the server 
-	err = server.ListenAndServe()
+	//Use the ListenAndServeTLS() method to start the HTTPS server. We
+	//pass in the paths to the TLS certificates and corresponding private key as
+	//the two parameters.
+	err = server.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	logger.Error(err.Error())
 	os.Exit(1)
 }
